abb4210aff26d57ec70c3d2955f10e4971ed124c
CuasarN1 <55405975+CuasarN1@users.noreply.github.com>
2021-01-31 03:18:28 +0400

    Переписал алгоритм определения цветов

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PIL import Image
 from random import choice
 
 path = "no/2020_02_17_11_53_37.png"
-eps = 15
+eps = 20
 min_col = 80
 max_col = 600
 
@@ -15,37 +15,27 @@ except FileNotFoundError:
     print("Файл не найден")
 
 img = img.convert('P', palette=Image.ADAPTIVE, colors = eps).convert("RGB")
+colors = Image.Image.getcolors(img)
 img.show()
 mode = img.mode
 size = img.size
-x, y = size[0], size[1]
+x = size[0]
+y = size[1]
 total = x * y * 1.0
 percent = round(total / 100)
 # print(x, y)
 
-ColorTable = {}
-for i in range(x):
-    for j in range(y):
-        # print(i, j)
-        clr = img.getpixel((i, j))
-        # if len(clr) == 4:
-        #    clr = (clr[0], clr[1], clr[2])
-        if ColorTable.get(clr) is None:
-            ColorTable[clr] = 1
-        else:
-            ColorTable[clr] += 1
-
-sortCLR = sorted(ColorTable.items(), reverse=True, key=lambda kv: kv[1])
+sortCLR = sorted(colors, reverse=True, key=lambda kv: kv[0])
 print(total, len(sortCLR), sortCLR)
 
 p = 100.00
 ans = {}
 for e in sortCLR:
-    curr = e[1] / total * 100
+    curr = e[0] / total * 100
     if mode == "RGBA":
-        col = (e[0][0], e[0][1], e[0][2])
+        col = (e[1][0], e[1][1], e[1][2])
     else:
-        col = e[0]
+        col = e[1]
     ans[col] = str(round(curr, 3)) + " %"
     p -= curr
 
