d1d03149513534b939af80f543406cf967929c6e
CuasarN1 <55405975+CuasarN1@users.noreply.github.com>
2021-05-14 00:02:28 +0400

    Conf build
    Последняя сборка перед конференцией.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from math import sqrt
 from PIL import Image
 import os
-# import tensorflow as tf
+from contextlib import redirect_stdout
+import tensorflow as tf
 from random import choice
+eps = 15
+min_col = 50
+max_col = 650
+borders = range(min_col, max_col)
 
-
-def test(arr):
+def test(arr, total, mode):
     p = 100.00
     ans = {}
     for e in arr:
@@ -31,52 +35,89 @@ def diff(arr):
         for j in range(i + 1, n):
             f, s = arr[i][1], arr[j][1]
             ans.append(sqrt(pow(f[0] - s[0], 2) + pow(f[1] - s[1], 2) + pow(f[2] - s[2], 2)))
-    return sorted(ans, reverse=True)
+    return ans
 
 
-d = "no"
-path_arr = []
-for path in os.listdir(d):
-    full_path = os.path.join(d, path)
-    if os.path.isfile(full_path):
-        path_arr.append(full_path)
-
-# path = "no/2020_02_17_11_53_37.png"
-eps = 15
-min_col = 50
-max_col = 650
-r = range(min_col, max_col)
-
-most = []
-
-for path in path_arr:
-
-    try:
-        img = Image.open(path, 'r')
-
-    except FileNotFoundError:
-        print("Файл не найден")
-
+def distance(img):
     img = img.convert('P', palette=Image.ADAPTIVE, colors=eps).convert("RGB")
     colors = Image.Image.getcolors(img)
-    # img.show()
-    mode = img.mode
-    size = img.size
-    x, y = size[0], size[1]
-    total = x * y * 1.0
-    percent = round(total / 100)
-    # print(x, y)
+    # mode = img.mode
+    # size = img.size
+    # total = size[0] * size[1] * 1.0
 
-    sortCLR = sorted(colors, reverse=True, key=lambda kv: kv[0])
-
-    trim = [c for c in sortCLR if c[1][0] + c[1][1] + c[1][2] in r]
+    # sortCLR = sorted(colors, reverse=True, key=lambda kv: kv[0])
+    # trim = list(filter(lambda c: c[1][0] + c[1][1] + c[1][2] in borders, sortCLR))
+    # d = diff(trim)
 
     # print(len(trim), trim)
     # print(len(sortCLR), sortCLR)
-    # test(sortCLR)
-    d = diff(trim)
+    # test(sortCLR, total, mode)
     # print(len(d), d)
 
-    most.append(d[0])
+    return sorted(diff(colors))
 
-print(sum(most) * 1.0 / len(most))
+
+folds = ['yes', 'no']
+for d in folds:
+    num = 0
+    path_arr = []
+    for path in os.listdir(d):
+        full_path = os.path.join(d, path)
+        if os.path.isfile(full_path):
+            path_arr.append(full_path)
+            num += 1
+
+    most = []
+    # for path in path_arr:
+    #    img = Image.open(path, 'r')
+    #    most.append(distance(img))
+
+    # print(sum(most) * 1.0 / len(most))
+
+    # hello = tf.constant('Hello, TensorFlow')
+    # sess = tf.compat.v1.Session()
+    # print(sess.run(hello))
+
+    file = []
+    max_min = -9999999999.0
+    max_max = -9999999999.0
+    max_avg = -9999999999.0
+    cnt1, cnt2 = 0, 0
+
+    for path in path_arr:
+        img = Image.open(path, 'r')
+        orig = distance(img)
+        most = []
+        file.append(path)
+        file.append('')
+        # for s in range(120, 180, 5):
+        r, g, b = img.split()
+        j = 1.45
+        r = r.point(lambda i: i * j)
+        g = g.point(lambda i: i * j)
+        b = b.point(lambda i: i * j)
+        result = Image.merge('RGB', (r, g, b))
+        new = distance(result)
+
+        omin, omax, oavg = min(orig), max(orig), sum(orig) / len(orig)
+        nmin, nmax, navg = min(new), max(new), sum(new) / len(new)
+        labs, rabs, aabs = abs(omin - nmin), abs(omax - nmax), abs(oavg - navg)
+        max_min, max_max, max_avg = max(max_min, labs), max(max_max, rabs), max(max_avg, aabs)
+        if labs >= 7.5: cnt1 += 1
+        if labs >= 9.0: cnt2 += 1
+
+        file.append('rgb shift: ' + str(j))
+        file.append('orig min, max, mid: ' + str(omin) + ' ; ' + str(omax) + ' ; ' + str(oavg))
+        file.append('new min, max, mid: ' + str(nmin) + ' ; ' + str(nmax) + ' ; ' + str(navg))
+        file.append('abs min max, mid: ' + str(labs) + ' ; ' + str(rabs) + ' ; ' + str(aabs))
+        file.append(120 * '-')
+
+    with open(d + '.txt', 'w') as f:
+        with redirect_stdout(f):
+            for line in file:
+                print(line)
+
+    print(d)
+    print(max_min, max_max, max_avg)
+    print(cnt2, cnt1)
+    print(num, '\n')
